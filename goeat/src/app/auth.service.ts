847afa4ba57b7abb6226b009309223aabc626f95
jorgeovaz <servaskyrb@gmail.com>
2020-05-21 13:17:15 +0200

    insercion firebase desde registro

--- goeat/src/app/auth.service.ts
+++ goeat/src/app/auth.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public afDB: AngularFireDatabase) { }
+  constructor(public afDB: AngularFirestore) { }
 
-  public crear_usuario(usuario){
-    console.log(usuario);
-    this.afDB.database.ref().set(usuario);
+  public crear_usuario(value){
+    console.log(value);
+    return this.afDB.collection('users').add({
+      email: value.email,
+      password: value.password
+    });
   }
-
-  
+ 
 }
