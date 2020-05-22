7502cc7b4f9ce1bdf0938e3f9267c24a95f63390
jorgeovaz <servaskyrb@gmail.com>
2020-05-22 10:28:52 +0200

    validación formulario

--- goeat/src/app/registro/registro.module.ts
+++ goeat/src/app/registro/registro.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -10,7 +10,7 @@ import { RegistroPage } from './registro.page';
 
 @NgModule({
   imports: [
-    CommonModule,
+    CommonModule, ReactiveFormsModule,
     FormsModule,
     IonicModule,
     RegistroPageRoutingModule
--- goeat/src/app/registro/registro.page.ts
+++ goeat/src/app/registro/registro.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from './usuario.model';
 import { AuthService } from '../auth.service';
-
-
-import { Router } from  "@angular/router";
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Router } from "@angular/router";
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -13,10 +14,20 @@ import { Router } from  "@angular/router";
 export class RegistroPage implements OnInit {
   usuario: Usuario=new Usuario();
   checked : boolean = false;
+  ionicForm: FormGroup;
+  isSubmitted = false;
 
-  constructor(public authService: AuthService) { }
+  constructor(private authService: AuthService, public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.ionicForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      password: ['', Validators.required]
+    })
+  }
+
+  get errorControl() {
+    return this.ionicForm.controls;
   }
 
   addValue(e): void {
@@ -24,8 +35,15 @@ export class RegistroPage implements OnInit {
   }
 
   registrar(){
-    console.log(this.usuario);
+    this.isSubmitted = true;
+    if (!this.ionicForm.valid) {
+      console.log('Inserta los datos bien!')
+      return false;
+    } else {
+      console.log(this.ionicForm.value)
+      console.log(this.usuario);
     this.authService.crear_usuario(this.usuario);
+    }
 
   }
 }
