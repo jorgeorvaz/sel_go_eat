44c68616eb3ad3bd0f70559b2bcf707ccbff3e18
crod27 <carlos271097@hotmail.com>
2020-05-25 11:28:02 +0200

    ERRORES CONTACTO

--- goeat/src/app/contacto/contacto.page.ts
+++ goeat/src/app/contacto/contacto.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Mensaje } from './contacto.model';
+
 
 @Component({
   selector: 'app-contacto',
@@ -6,16 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacto.page.scss'],
 })
 export class ContactoPage implements OnInit {
+  contacto: Mensaje = new Mensaje();
+  ionicForm: FormGroup;
+  isSubmitted = false;
 
-  constructor() { }
+  constructor(private authService: AuthService, public formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    
+    this.ionicForm = this.formBuilder.group({
+      nombre : ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+    })
 
 
   }
 
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
+
   enviar_mensaje(){
+    this.isSubmitted = true;
+    if (!this.ionicForm.valid) {
+      console.log('Inserta los datos bien!')
+      return false;
+    } else {
+      console.log(this.ionicForm.value)
+      console.log(this.contacto);
+      this.authService.iniciar_usuario(this.contacto);
+      
+    }
 
 
   }
