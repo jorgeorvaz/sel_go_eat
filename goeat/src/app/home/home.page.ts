2e32a80dcd9610d0da70dadf5c1970e3dc5f11fa
jorgeovaz <servaskyrb@gmail.com>
2020-06-05 10:30:36 +0200

    getdistance func

--- goeat/src/app/home/home.page.ts
+++ goeat/src/app/home/home.page.ts
@@ -281,4 +281,34 @@ export class HomePage implements OnInit {
     console.log('URL: ', url);
     this.iab.create(url, '_system');
   }
+
+  getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = this.deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+  }
+  
+  deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }
+
+private setCurrentLocation() {
+    let my_coords:Array<any> = [];
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        my_coords[0] = position.coords.latitude;
+        my_coords[1] = position.coords.longitude;
+
+      });
+    }
+    return my_coords;
+  }
 }
